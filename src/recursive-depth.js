a46b41fcf6768b7e7ccc8806cef10904561e5fa3
ssekridov@gmail.com <ssekridov@gmail.com>
2022-10-17 00:33:02 +0200

    feat: sum-digits done

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,9 +3,9 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let count = 0
+    arr.forEach(element => {
+      if (Array.isArray(element)) {
+        count = count + 1
+        this.calculateDepth(element)
+      }
+    })
+    return count
   }
 }
 
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const getSumOfDigits = (n) => {
+  let sum = 0
+  let arr = Array.from('' + n).map(Number)
+  arr.forEach(element => {
+  sum += element
+  })
+  return (sum + '').length > 1 ? getSumOfDigits(sum) : sum
 }
 
 module.exports = {
